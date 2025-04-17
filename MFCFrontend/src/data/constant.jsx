87ffa84fc42a1b100aef2f73c44b7453dc42748f
reseter1 <nguyenhuutai.reseter@gmail.com>
2025-04-17 14:55:54 +0000

    Add deploy workflow.

--- MFCFrontend/src/data/constant.jsx
+++ MFCFrontend/src/data/constant.jsx
@@ -1,5 +1,5 @@
 export const APP_NAME = "MFC";
 export const APP_VERSION = "0.1.0";
-export const API_URL = " http://localhost:5277";
+export const API_URL = "https://mfc-backend.servernux.com";
 export const API_URL_GENAI = "https://genai-reseter.servernux.com/api";
 export const PROVIDER_URL = "https//huutai-dev.hackquest.com";
